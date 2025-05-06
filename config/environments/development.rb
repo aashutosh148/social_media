81ba2f0dd6241dd31d676bb0ff3eca923eeeaa2a
Aashutosh Singh <aashutoshsingh468847@gmail.com>
2025-05-06 11:16:26 +0530

    fix

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -72,8 +72,8 @@ Rails.application.configure do
     address: 'smtp.gmail.com',
     port: 587,
     domain: 'gmail.com',
-    user_name: 'ashutoshsingh468847', # Your actual email
-    password: 'syimiclbxyjunfrd', # Your actual password/app password
+    user_name: '', # Your actual email
+    password: '', # Your actual password/app password
     authentication: 'plain',
     enable_starttls_auto: true
   }
